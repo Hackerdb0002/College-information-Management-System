73863f50ac79082214474384b4fb6eab64adb75b
Hackerdb0002 <88364760+Hackerdb0002@users.noreply.github.com>
2021-09-28 21:14:35 +0530

    Add files via upload

---
+++ College-information-Management-System/AboutManagement.php
@@ -0,0 +1,28 @@
+<!Doctype>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css"/>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/home.css" />
+        <title>This project is develope by Projectworlds.in</title>
+    </head>
+    <body>
+        <div class="panel panel-default">
+            <div class="panel-heading"><h1 style="text-align: center;"><span class="glyphicon glyphicon-user"></span> About Project!</h1></div>
+  			<div class="panel-body">
+			<div class="container">
+				<p style="text-align:center;">Projectworlds.</p>
+                        </div><br><br>
+                        
+                        <h1><strong>Project title: </strong>College Information Management System</h1>
+                        <h1><strong>Made by: </strong>Dhiraj Bhokare.</h1>
+                        <h1><strong>College: </strong>Cocsit college latur.</h1>
+                        <h1><strong>Branch: </strong>Software engineering.</h1>
+						<h1><strong>Mo No.: </strong>7038648397</h1>
+                        </div>
+        </div>
+    </body>
+</html>
